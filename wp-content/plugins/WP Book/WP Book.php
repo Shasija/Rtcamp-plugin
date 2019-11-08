f63e61c9fc3c67c03ce7b707dcbda607dca5a7f9
Shasija <hasijasankalp@gmail.com>
2019-11-08 17:07:42 +0530

    plugin created

---
+++ wp-content/plugins/WP Book/WP Book.php
@@ -0,0 +1,11 @@
+<?php
+/*
+* Plugin Name: WP Book
+* Description: Add, Update, Delete and Display
+* Plugin URI: https://sankalphasija.wordpress.com/
+* Author: Sankalp Hasija
+* Author URI: https://sankalphasija.wordpress.com/
+* Version: 1.0.0
+*/
+
+?>
